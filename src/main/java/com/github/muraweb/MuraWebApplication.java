da54f8b724d42ac8d430f68d54a9d560bbdfcfa2
ZhongXiLu <zhongxilu@hotmail.com>
2020-08-08 10:46:16 +0200

    Initial commit

---
+++ src/main/java/com/github/muraweb/MuraWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.muraweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuraWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/muraweb/MuraWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.muraweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuraWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
