8ae9afcde2858dde1dcf82946b9f8841bbcb7469
ZhongXiLu <zhongxilu@hotmail.com>
2020-08-11 15:55:36 +0200

    Added basic test

---
+++ src/test/java/com/github/muraweb/AnalysisTest.java
@@ -0,0 +1,45 @@
+package com.github.muraweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebMvcTest(AnalysisController.class)   // Mock server
+class AnalysisTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAnalysisTest() throws Exception {
+        RequestBuilder getAnalysisRequest = MockMvcRequestBuilders.get("/analysis");
+        MvcResult result = mockMvc.perform(getAnalysisRequest).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void postAnalysisTest() throws Exception {
+        RequestBuilder getAnalysisRequest = MockMvcRequestBuilders
+                .post("/analysis")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("gitDir","https://github.com/OpenFeign/feign")
+                .param("singleModule", "false")
+                .param("module", "core")
+                .param("CK", "true")
+                .param("CC", "true")
+                .param("USG", "true")
+                .param("H", "true")
+                .param("LC", "true")
+                .param("IMP", "false");
+        MvcResult result = mockMvc.perform(getAnalysisRequest).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+    }
+
+}
